b1fee9bd9fac3d258a102bd39f43ad7247054254
19Amy <amygathonii@gmail.com>
2023-04-25 18:40:57 +0300

    Added a from and created  search function

--- js/index.js
+++ js/index.js
@@ -19,26 +19,35 @@ function formatDate(timestamp){
 function displayTemperature(response){
     let dateElement =  document.querySelector('#time')
     let iconElement = document.querySelector('#icon')
-    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}10d@2x.png`) 
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}04d@2x.png`) 
     iconElement.setAttribute("alt", response.data.weather[0].description); 
     document.querySelector('#temp').innerHTML = Math.round(response.data.main.temp);
     document.querySelector('#description').innerHTML = response.data.weather[0].description;
     document.querySelector('#humidity').innerHTML= 'humidity: '+response.data.main.humidity + '%';
     document.querySelector('#wind').innerHTML= 'wind: ' +response.data.wind.speed +'km/h';
-    document.querySelector('#precipitation').innerHTML = 'Precipitation:';
+    document.querySelector('#precipitation').innerHTML = ' ';
     dateElement.innerHTML = formatDate(response.data.dt * 1000); //converts timestamp into milliseconds
     console.log(response.data)
 }
 
-
-
-
-
-
-
-let city = 'New York';
-let apiKey = '08b30899ed0fccd0945fbcd0779a8eac';
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city){
+    let apiKey = '08b30899ed0fccd0945fbcd0779a8eac';
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
    
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
 
+}
+
+
+function handleSubmit(event){
+    event.preventDefault();
+    cityInputElement = document.querySelector('#city-input');
+    search(cityInputElement.value)
+    document.querySelector('h1').innerHTML = cityInputElement.value
+}
+ 
+search('New York')
+
+
+let form = document.querySelector('#search-form');
+form.addEventListener('submit', handleSubmit)
