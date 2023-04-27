1c20dee7b2c6db2be6d7ee5b01c21200cf5f061a
19Amy <amygathonii@gmail.com>
2023-04-27 15:50:05 +0300

    calling forecast API

--- js/index.js
+++ js/index.js
@@ -15,7 +15,8 @@ function formatDate(timestamp){
 }
 
 
-function displayForecast(){
+function displayForecast(response){
+    
     let forecastElement = document.querySelector('#forecast');
 
     let forecastHtml = `<div class="row">`
@@ -49,6 +50,14 @@ function displayForecast(){
 }
 
 
+function getForecast(coordinates){
+    let apiKey = '08b30899ed0fccd0945fbcd0779a8eac';
+    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+    console.log(apiUrl)
+    axios.get(apiUrl).then(displayForecast)
+}
+
+
 function displayTemperature(response){
     let dateElement =  document.querySelector('#time');
     let iconElement = document.querySelector('#icon');
@@ -70,6 +79,10 @@ function displayTemperature(response){
     document.querySelector('#wind').innerHTML= 'wind: ' +response.data.wind.speed +'km/h';
     document.querySelector('#precipitation').innerHTML = ' ';
    
+
+    getForecast(response.data.coord);
+
+
     console.log(response.data)
 }
 
